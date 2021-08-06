46a92c210ee254d825060958205e82d064f77436
gustavo <gustavo.isaac@gmail.com>
2021-08-06 20:12:35 +0100

    Navigation

--- app/src/main/java/com/artemissoftware/arexionmessages/ui/MainActivity.kt
+++ app/src/main/java/com/artemissoftware/arexionmessages/ui/MainActivity.kt
@@ -2,11 +2,41 @@ package com.artemissoftware.arexionmessages.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.artemissoftware.arexionmessages.R
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        navController = findNavController(R.id.navHostFragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.arexionFragment,
+                R.id.xenophonFragment
+            )
+        )
+
+
+        bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
