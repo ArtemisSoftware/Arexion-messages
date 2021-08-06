aa185c172285b3552c7ca4475a4485856a8b40f7
gustavo <gustavo.isaac@gmail.com>
2021-08-06 18:34:57 +0100

    Navigation graph

--- app/src/main/java/com/artemissoftware/arexionmessages/ui/MainActivity.kt
+++ app/src/main/java/com/artemissoftware/arexionmessages/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.artemissoftware.arexionmessages
+package com.artemissoftware.arexionmessages.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.artemissoftware.arexionmessages.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
