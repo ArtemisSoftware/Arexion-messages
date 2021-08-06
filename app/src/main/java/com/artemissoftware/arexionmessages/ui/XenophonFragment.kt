e17351861d1b3566395c9ead5d95a130766f478c
gustavo <gustavo.isaac@gmail.com>
2021-08-06 18:14:20 +0100

    Layout fragment_xenophon.xml

---
+++ app/src/main/java/com/artemissoftware/arexionmessages/ui/XenophonFragment.kt
@@ -0,0 +1,17 @@
+package com.artemissoftware.arexionmessages.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.artemissoftware.arexionmessages.R
+
+
+class XenophonFragment : Fragment(R.layout.fragment_xenophon) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
