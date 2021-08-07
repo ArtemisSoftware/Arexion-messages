5ecab85a3458ee1feef3e8ccdb0ece2ba60116f3
gustavo <gustavo.isaac@gmail.com>
2021-08-07 13:58:27 +0100

    Lifecycle, ViewModel & LiveData dependencies

--- app/src/main/java/com/artemissoftware/arexionmessages/ui/ArexionFragment.kt
+++ app/src/main/java/com/artemissoftware/arexionmessages/ui/ArexionFragment.kt
@@ -5,15 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.artemissoftware.arexionmessages.R
+import com.artemissoftware.arexionmessages.ui.adapters.PredictionListAdapter
+import com.artemissoftware.arexionmessages.ui.models.Prediction
+import kotlinx.android.synthetic.main.fragment_arexion.*
 
 
 class ArexionFragment : Fragment(R.layout.fragment_arexion) {
 
+
+    private val predictionListAdapter by lazy { PredictionListAdapter() }
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setupRecyclerView()
+
+        predictionListAdapter.submitList(listOf(Prediction("Prediction 2"), Prediction("Prediction 1")))
 
     }
 
 
+
+    private fun setupRecyclerView() {
+        recyclerView.adapter = predictionListAdapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+    }
 }
